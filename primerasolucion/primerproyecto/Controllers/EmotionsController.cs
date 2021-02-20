79fedff80f2af0aad508f13823e6eb7f53a9c37a
Emily <emi.roca18@gmail.com>
2021-02-20 11:41:49 -0400

    emocion sin modelo

---
+++ primerasolucion/primerproyecto/Controllers/EmotionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primerproyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmotionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConNumero")]
+        public string Emocion (int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona indiferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado no te puedo atender en este momento";
+        }
+        [HttpPost]
+
+        public string Emocion2 ([FromHeader] int suerte)
+        {
+            if (suerte == 0)
+                return "sos una persona indiferente";
+            if (suerte == 1)
+                return "sos una persona feliz";
+            if (suerte == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado no te puedo atender en este momento";
+        }
+
+
+    }
+}
